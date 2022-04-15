31951464f00c17ab877b8f693e62b6f98e602b6d
Captain-America-X <103486323+Captain-America-X@users.noreply.github.com>
2022-04-15 18:47:18 +0530

    Update __init__.py

--- alita/__init__.py
+++ alita/__init__.py
@@ -68,11 +68,11 @@ except Exception as ef:
     sysexit(1)
 
 LOGGER.info("------------------------")
-LOGGER.info("|      Alita_Robot     |")
+LOGGER.info("|      SK 2.0    |")
 LOGGER.info("------------------------")
 LOGGER.info(f"Version: {Config.VERSION}")
 LOGGER.info(f"Owner: {str(Config.OWNER_ID)}")
-LOGGER.info("Source Code: https://github.com/DivideProjects/Alita_Robot\n")
+LOGGER.info("Source Code: https://github.com/Captain-America-X/SK2.0\n")
 
 # Account Related
 BOT_TOKEN = Config.BOT_TOKEN
